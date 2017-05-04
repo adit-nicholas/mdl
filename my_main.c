c1cde91c92a480a38a576447594374b47a930833
adit-nicholas <adit.nicholas@gmail.com>
2017-05-04 11:00:47 -0400

    Add files via upload

---
+++ my_main.c
@@ -0,0 +1,133 @@
+/*========== my_main.c ==========
+
+  This is the only file you need to modify in order
+  to get a working mdl project (for now).
+
+  my_main.c will serve as the interpreter for mdl.
+  When an mdl script goes through a lexer and parser, 
+  the resulting operations will be in the array op[].
+
+  Your job is to go through each entry in op and perform
+  the required action from the list below:
+
+  push: push a new origin matrix onto the origin stack
+  pop: remove the top matrix on the origin stack
+
+  move/scale/rotate: create a transformation matrix 
+                     based on the provided values, then 
+		     multiply the current top of the
+		     origins stack by it.
+
+  box/sphere/torus: create a solid object based on the
+                    provided values. Store that in a 
+		    temporary matrix, multiply it by the
+		    current top of the origins stack, then
+		    call draw_polygons.
+
+  line: create a line based on the provided values. Store 
+        that in a temporary matrix, multiply it by the
+	current top of the origins stack, then call draw_lines.
+
+  save: call save_extension with the provided filename
+
+  display: view the image live
+  
+  jdyrlandweaver
+  =========================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "parser.h"
+#include "symtab.h"
+#include "y.tab.h"
+
+#include "matrix.h"
+#include "ml6.h"
+#include "display.h"
+#include "draw.h"
+#include "stack.h"
+
+void my_main() {
+
+  int i;
+  struct matrix *tmp,*mat;
+  struct stack *s;
+  screen t;
+  color g;
+  double step = 0.1;
+  
+  s = new_stack();
+  tmp = new_matrix(4, 1000);
+  clear_screen( t );
+
+  for (i=0;i<lastop;i++) {  
+    switch (op[i].opcode) {
+    case 'push':
+      push(s);
+      break;
+    case 'pop':
+      pop(s);
+      break;
+    case 'move':
+      mat = make_translate(op[i].op.move.d[0],op[i].op.move.d[1]
+					   ,op[i].op.move.d[2]);
+      matrix_mult(peek(s),mat);
+      copy_matrix(mat,peek(s));
+      free_matrix(mat);
+      break;
+    case 'scale':
+      mat = make_scale(op[i].op.scale.d[0],op[i].op.scale.d[1],
+		       op[i].op.scale.d[2]);
+      matrix_mult(peek(s),mat);
+      copy_matrix(mat,peek(s));
+      free_matrix(mat);
+      break;
+    case 'rotate':
+      char axis =  &op[i].op.rotate.axis;
+      double theta = &op[i].op.rotate.degrees * (M_PI/180);
+      if (axis = 'x')
+	mat = make_rotX(theta);
+    case 'sphere':
+      add_sphere(tmp, op[i].op.sphere.d[0],op[i].op.sphere.d[1],
+		 op[i].op.sphere.d[2],
+		 op[i].op.sphere.r,step);
+      matrix_mult(peek(s),tmp);
+      draw_polygons(edges,s,c);
+      tmp->lastcol = 0;
+      break;
+    case 'box':
+      add_box(tmp, op[i].op.box.d0[0],op[i].op.box.d0[1],
+	      op[i].op.box.d0[2],
+	      op[i].op.box.d1[0],op[i].op.box.d1[1],
+	      op[i].op.box.d1[2]);
+      matrix_mult(peek(s),tmp);
+      draw_polygons(tmp,s,c);
+      tmp -> lastcol = 0;
+      break;
+    case 'torus':
+      add_torus(tmp,op[i].op.torus.d[0],op[i].op.torus.d[1],
+		op[i].op.torus.d[2],
+		op[i].op.torus.r0,op[i].op.torus.r1,step);
+      matrix_mult(peek(s),tmp);
+      draw_polygons(edges,s,c);
+      tmp->lastcol = 0;
+      break;
+    case 'line':
+      add_edge(tmp,op[i].op.line.p0[0],op[i].op.line.p0[1],
+	       op[i].op.line.p0[1],
+	       op[i].op.line.p1[0],op[i].op.line.p1[1],
+	       op[i].op.line.p1[1]);
+      matrix_mult(peek(s),tmp);
+      draw_lines(tmp,s,c);
+      tmp->lastcol = 0;
+      break;
+    case 'save':
+    case 'display':
+    default:
+      printf("Invalid command\n");
+   
+    }
+  }
+}
